950c5edc1dbc9c19fcb23d683af00bc5251f7330
Stephen Coogan <hello@coog.ie>
2015-07-12 18:44:13 +0100

    Setup routes, templates, controllers, and wire it all up

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('simpleluas', ['ionic'])
+angular.module('simpleluas', ['ionic', 'simpleluas.controllers'])
 
 .run(['$ionicPlatform', function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -17,3 +17,13 @@ angular.module('simpleluas', ['ionic'])
         }
     });
 }])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/home',
+        controller: 'HomeCtrl',
+        templateUrl: 'templates/home.html' 
+    });
+
+    $urlRouterProvider.otherwise('/home');
+}]);
