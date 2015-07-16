ff4fb466a7e70a266da52907488a73cc793b328c
Stephen Coogan <hello@coog.ie>
2015-07-16 22:32:06 +0100

    Add station select and filter them according to selected line

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,28 @@
 angular.module('simpleluas.controllers', [])
 
-.controller('HomeCtrl', ['$rootScope', '$scope', 'Stations', function($rootScope, $scope, Stations) {
+.controller('HomeCtrl', ['$rootScope', '$scope', '$filter', 'Stations', function($rootScope, $scope, $filter, Stations) {
+    var allStations;
     $scope.activeLine = $rootScope.defaultActiveLine;
 
+    function updateStationList() {
+        $scope.stations = $filter('filter')(allStations, $scope.activeLine);
+        $scope.selectedStation = $scope.stations[0];
+        console.log('updateStationList(), ', $scope.stations, $scope.selectedStation);
+    }
+
     Stations.getAll().then(function(data) {
-        $scope.stations = data.stations;
-        console.log('Stations.getAll() initialisation, ', $scope.stations);
+        allStations = data.stations;
+        console.log('Stations.getAll() initialisation, ', allStations);
+        updateStationList();
     });
 
-    $scope.isActiveLine = function (line) {
+    $scope.isActiveLine = function(line) {
         return line === $scope.activeLine;
     };
 
-    $scope.setActiveLine = function (line) {
+    $scope.setActiveLine = function(line) {
         $scope.activeLine = line.toLowerCase();
         console.log('setActiveLine(),', $scope.activeLine);
+        updateStationList();
     };
 }]);
